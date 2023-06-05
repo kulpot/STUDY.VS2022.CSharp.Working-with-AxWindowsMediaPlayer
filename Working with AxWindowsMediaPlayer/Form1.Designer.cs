28eb0c052b4a2cc1e37e04f9fdc1c9c747985724
Kulpot <kulpotwahu@gmail.com>
2023-06-05 13:10:35 +0800

    Add project files.

---
+++ Working with AxWindowsMediaPlayer/Form1.Designer.cs
@@ -0,0 +1,138 @@
+﻿namespace Working_with_AxWindowsMediaPlayer
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.btnStop = new System.Windows.Forms.Button();
+            this.btnStart = new System.Windows.Forms.Button();
+            this.btnFullScreen = new System.Windows.Forms.Button();
+            this.lblPath = new System.Windows.Forms.Label();
+            this.btnPickFile = new System.Windows.Forms.Button();
+            this.mediaPlayer = new AxWMPLib.AxWindowsMediaPlayer();
+            ((System.ComponentModel.ISupportInitialize)(this.mediaPlayer)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // btnStop
+            // 
+            this.btnStop.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Left)));
+            this.btnStop.Location = new System.Drawing.Point(12, 253);
+            this.btnStop.Name = "btnStop";
+            this.btnStop.Size = new System.Drawing.Size(75, 23);
+            this.btnStop.TabIndex = 1;
+            this.btnStop.Text = "Stop";
+            this.btnStop.UseVisualStyleBackColor = true;
+            this.btnStop.Click += new System.EventHandler(this.btnStop_Click);
+            // 
+            // btnStart
+            // 
+            this.btnStart.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Left)));
+            this.btnStart.Location = new System.Drawing.Point(93, 253);
+            this.btnStart.Name = "btnStart";
+            this.btnStart.Size = new System.Drawing.Size(75, 23);
+            this.btnStart.TabIndex = 2;
+            this.btnStart.Text = "Start";
+            this.btnStart.UseVisualStyleBackColor = true;
+            this.btnStart.Click += new System.EventHandler(this.btnStart_Click);
+            // 
+            // btnFullScreen
+            // 
+            this.btnFullScreen.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Left)));
+            this.btnFullScreen.Location = new System.Drawing.Point(174, 253);
+            this.btnFullScreen.Name = "btnFullScreen";
+            this.btnFullScreen.Size = new System.Drawing.Size(75, 23);
+            this.btnFullScreen.TabIndex = 3;
+            this.btnFullScreen.Text = "FullScreen";
+            this.btnFullScreen.UseVisualStyleBackColor = true;
+            this.btnFullScreen.Click += new System.EventHandler(this.btnFullScreen_Click);
+            // 
+            // lblPath
+            // 
+            this.lblPath.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.lblPath.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.lblPath.Location = new System.Drawing.Point(12, 284);
+            this.lblPath.Name = "lblPath";
+            this.lblPath.Size = new System.Drawing.Size(315, 23);
+            this.lblPath.TabIndex = 4;
+            this.lblPath.Text = "Select a File";
+            this.lblPath.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // btnPickFile
+            // 
+            this.btnPickFile.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Right)));
+            this.btnPickFile.Location = new System.Drawing.Point(333, 284);
+            this.btnPickFile.Name = "btnPickFile";
+            this.btnPickFile.Size = new System.Drawing.Size(50, 23);
+            this.btnPickFile.TabIndex = 5;
+            this.btnPickFile.Text = "...";
+            this.btnPickFile.UseVisualStyleBackColor = true;
+            this.btnPickFile.Click += new System.EventHandler(this.btnPickFile_Click);
+            // 
+            // mediaPlayer
+            // 
+            this.mediaPlayer.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.mediaPlayer.Enabled = true;
+            this.mediaPlayer.Location = new System.Drawing.Point(12, 12);
+            this.mediaPlayer.Name = "mediaPlayer";
+            this.mediaPlayer.OcxState = ((System.Windows.Forms.AxHost.State)(resources.GetObject("mediaPlayer.OcxState")));
+            this.mediaPlayer.Size = new System.Drawing.Size(362, 235);
+            this.mediaPlayer.TabIndex = 0;
+            this.mediaPlayer.PlayStateChange += new AxWMPLib._WMPOCXEvents_PlayStateChangeEventHandler(this.mediaPlayer_PlayStateChange);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(385, 316);
+            this.Controls.Add(this.btnPickFile);
+            this.Controls.Add(this.lblPath);
+            this.Controls.Add(this.btnFullScreen);
+            this.Controls.Add(this.btnStart);
+            this.Controls.Add(this.btnStop);
+            this.Controls.Add(this.mediaPlayer);
+            this.Name = "Form1";
+            this.Text = "Media Player";
+            ((System.ComponentModel.ISupportInitialize)(this.mediaPlayer)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private AxWMPLib.AxWindowsMediaPlayer mediaPlayer;
+        private System.Windows.Forms.Button btnStop;
+        private System.Windows.Forms.Button btnStart;
+        private System.Windows.Forms.Button btnFullScreen;
+        private System.Windows.Forms.Label lblPath;
+        private System.Windows.Forms.Button btnPickFile;
+    }
+}
+
---
+++ Working with AxWindowsMediaPlayer/Form1.cs
@@ -0,0 +1,76 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+//ref link:https://www.youtube.com/watch?v=KXi3KQiMqGw&list=PLAIBPfq19p2EJ6JY0f5DyQfybwBGVglcR&index=61
+// media player for mp3 and videos .....
+
+namespace Working_with_AxWindowsMediaPlayer
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+            //mediaPlayer.uiMode = "none"; // ="" (none, mini, invisible, full) full is the default
+            //mediaPlayer.ShowPropertyPages(); // media settings
+            //mediaPlayer.settings.autoStart = false; // auto start off
+            //mediaPlayer.settings.mute = true; // starts up muted
+            mediaPlayer.settings.volume = 75; // sounds 75%
+
+        }
+
+        private void btnPickFile_Click(object sender, EventArgs e)
+        {
+            using (OpenFileDialog dlgOpenFile = new OpenFileDialog())
+            {
+                dlgOpenFile.Filter = "All Files|*.*";
+                
+                if(dlgOpenFile.ShowDialog() == System.Windows.Forms.DialogResult.OK)
+                {
+                    try
+                    {
+                        mediaPlayer.URL = dlgOpenFile.FileName;
+                        lblPath.Text = dlgOpenFile.FileName;
+                    }
+                    catch(Exception ex)
+                    {
+                        MessageBox.Show("Unable to load file!\n\n" + ex.Message);
+                    }
+                }
+            } 
+        }
+
+        private void btnStop_Click(object sender, EventArgs e)
+        {
+            mediaPlayer.Ctlcontrols.stop();
+        }
+
+        private void btnStart_Click(object sender, EventArgs e)
+        {
+            mediaPlayer.Ctlcontrols.play();
+        }
+
+        private void btnFullScreen_Click(object sender, EventArgs e)
+        {
+            if(mediaPlayer.URL.Length > 0)
+            {
+                mediaPlayer.fullScreen = true;
+            }
+        }
+
+        private void mediaPlayer_PlayStateChange(object sender, AxWMPLib._WMPOCXEvents_PlayStateChangeEvent e)
+        {
+            if(mediaPlayer.playState == WMPLib.WMPPlayState.wmppsStopped) // repeat play more than once
+            {
+                mediaPlayer.Ctlcontrols.play(); 
+            }
+        }
+    }
+}
